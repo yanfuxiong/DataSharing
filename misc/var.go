1b4f0253153c6e0a2f8ca1861f7a2ca4e16e236a
yanfuxiong <40554272+yanfuxiong@users.noreply.github.com>
2025-03-20 13:00:23 +0800

    Create var.go

---
+++ misc/var.go
@@ -0,0 +1,10 @@
+package misc
+
+const (
+	LanServerName   = "GoZeroconfLanServer" // TODO: DIAS mac address
+	LanServiceType  = "_rtkcs._tcp"
+	LanServerDomain = "local"
+	LanServerPort   = 42424
+
+	ClientHeartbeatInterval = 10 // second
+)
