b4d1acc7101f3e1656566cb7e7df23d969f470ee
yanfuxiong <40554272+yanfuxiong@users.noreply.github.com>
2025-04-15 09:46:04 +0800

    Update misc.go

--- misc/misc.go
+++ misc/misc.go
@@ -1,6 +1,7 @@
 package misc
 
 import (
+	"errors"
 	"fmt"
 	"gopkg.in/natefinch/lumberjack.v2"
 	"log"
@@ -95,7 +96,91 @@ func FileExists(filename string) bool {
 	return !info.IsDir()
 }
 
+func FolderExists(folder string) bool {
+	info, err := os.Stat(folder)
+	if os.IsNotExist(err) {
+		return false
+	}
+	return info.IsDir()
+}
+
+func FileSize(filePath string) (uint64, error) {
+	fileInfo, err := os.Stat(filePath)
+	if err != nil {
+		if os.IsNotExist(err) {
+			log.Printf("File %s does not exist\n", filePath)
+		} else {
+			log.Printf("Getting file:[%s] info error: %+v\n", filePath, err)
+		}
+		return 0, err
+	}
+
+	if fileInfo.IsDir() {
+		log.Printf("File %s is a directory!", filePath)
+		return 0, errors.New("this is a directory")
+	}
+
+	return uint64(fileInfo.Size()), nil
+}
+
+func FileSizeDesc(size uint64) string {
+	const (
+		B = 1 << (10 * iota)
+		KB
+		MB
+		GB
+		TB
+	)
+
+	switch {
+	case size >= TB:
+		return fmt.Sprintf("%.2f TB", float64(size)/float64(TB))
+	case size >= GB:
+		return fmt.Sprintf("%.2f GB", float64(size)/float64(GB))
+	case size >= MB:
+		return fmt.Sprintf("%.2f MB", float64(size)/float64(MB))
+	case size >= KB:
+		return fmt.Sprintf("%.2f KB", float64(size)/float64(KB))
+	default:
+		return fmt.Sprintf("%d B", size)
+	}
+}
+
 func ConcatIP(ip string, port string) string {
 	publicIP := fmt.Sprintf("%s:%s", ip, port)
 	return publicIP
+}
+
+func IsInTheList(target string, list []string) bool {
+	for _, item := range list {
+		if strings.EqualFold(item, target) {
+			return true
+		}
+	}
+	return false
+}
+
+func RemoveStringFromSlice(slice []string, s string) []string {
+	i := 0
+	for _, v := range slice {
+		if v != s {
+			slice[i] = v
+			i++
+		}
+	}
+	return slice[:i]
+}
+
+func CreateDir(dir string, dirMode os.FileMode) error {
+	_, err := os.Stat(dir)
+
+	if os.IsNotExist(err) {
+		err = os.MkdirAll(dir, dirMode)
+		if err != nil {
+			return fmt.Errorf("failed to create directory:[%s] error: %+v", dir, err)
+		}
+	} else if err != nil {
+		return fmt.Errorf("failed to check directory[%s] error: %+v", dir, err)
+	}
+	return nil
 }
