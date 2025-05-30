520b5ddfeb659580aa5b35d42b4803d03a167167
xiongyanfu <fg2009_0453@126.com>
2025-05-30 11:36:07 +0800

    update script

--- build_windows.ps1
+++ build_windows.ps1
@@ -5,16 +5,13 @@ $clipboardLib = "libclipboard.a"
 $clientGo = "main\client_main.go"
 
 # go build param
-$version = "2.0.8"
+$version = "2.1.16"
 $buildDate = (Get-Date).ToString("yyyy-MM-ddTHH:mm:ss")
-$platform = "windows"
-$isHiddenWin = $true
-if ($isHiddenWin) {
-    $ldflags = "-X rtk-cross-share/buildConfig.Version=$version -X rtk-cross-share/buildConfig.Platform=$platform -X rtk-cross-share/buildConfig.BuildDate=$buildDate -H=windowsgui"
-} else {
-    $ldflags = "-X rtk-cross-share/buildConfig.Version=$version -X rtk-cross-share/buildConfig.Platform=$platform -X rtk-cross-share/buildConfig.BuildDate=$buildDate"
-}
+$isBuildRelease = $true
+$isBuildDebug = $true
 
+$ldflags_release = "-X rtk-cross-share/client/buildConfig.Version=$version -X rtk-cross-share/client/buildConfig.BuildDate=$buildDate -X rtk-cross-share/client/buildConfig.Debug=0 -H=windowsgui"
+$ldflags_debug = "-X rtk-cross-share/client/buildConfig.Version=$version -X rtk-cross-share/client/buildConfig.BuildDate=$buildDate -X rtk-cross-share/client/buildConfig.Debug=1"
 
 if (-Not (Test-Path -Path $buildFolder)) {
     mkdir $buildFolder
@@ -24,7 +21,7 @@ Write-Host "Compile Start"
 foreach ($file in $cppFiles) {
     $outfile_o = Join-Path $buildFolder ($file.BaseName + ".o")
     Write-Host "Compiling $($file.FullName) to $outfile_o"
-    g++ -c $file.FullName -o $outfile_o
+    g++ -I "clipboard/pipeserver-asio" -I "clipboard/asio-1.30.2" -c $file.FullName -o $outfile_o
 }
 
 Write-Host "Compiling all .o to $buildFolder\$clipboardLib"
@@ -32,11 +29,21 @@ ar rcs $buildFolder\$clipboardLib $buildFolder\*.o
 rm $buildFolder\*.o
 Write-Host "Compiling ($clientGo) to client_windows.exe"
 cd "client"
-go build `
-    -ldflags "$ldflags" `
-    -a `
-    -o client_windows.exe `
-    $clientGo
+if ($isBuildRelease) {
+    go build `
+        -ldflags "$ldflags_release" `
+        -a `
+        -o client_windows_release.exe `
+        $clientGo
+}
+
+if ($isBuildDebug) {
+    go build `
+        -ldflags "$ldflags_debug" `
+        -a `
+        -o client_windows_debug.exe `
+        $clientGo
+}
 
 cd ..
 Write-Host "Compile Done"
